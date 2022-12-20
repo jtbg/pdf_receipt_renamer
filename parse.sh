2ceef0979bad3469ec00520cd4ab97b2ebe200e4
Justin Bassett-Green <justin@bassett.green>
2022-12-20 03:35:04 -0600

    multi-year support

--- parse.sh
+++ parse.sh
@@ -7,7 +7,7 @@ for file in *.pdf; do
   # Convert PDF to plain text
   pdftotext "$file" "${file%.pdf}.txt"
 
-  raw_date=$(awk '/[0-1][0-9]\/[0-3][0-9]\/2022/ {print $1}' "${file%.pdf}.txt")
+  raw_date=$(awk '/[0-1][0-9]\/[0-3][0-9]\/20[0-9]{2}/ {print $1}' "${file%.pdf}.txt")
   formatted_date=$(date -d "$raw_date" +%Y-%m-%d)
 
   # URLs, excluding emails
